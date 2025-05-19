e41869c75c6d4ff16a206a30ef52397230b0014b
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-05-19 23:09:00 +0800

    Add setting migration

--- database/migration/setting.go
+++ database/migration/setting.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"gin-boilerplate/internal/utilities"
 	"gorm.io/gorm"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -29,4 +31,48 @@ func DropSettingTable(db *gorm.DB) error {
 
 	fmt.Println("Drop Setting Table")
 	return nil
+}
+
+func (s *Setting) ConvertValue() interface{} {
+	switch strings.ToLower(s.Type) {
+	case "string":
+		if s.Value == nil {
+			return nil
+		}
+		if s.Value.Valid == false {
+			return nil
+		}
+		return s.Value.String
+	case "integer":
+		if s.Value == nil {
+			return int64(0)
+		}
+		val, _ := strconv.ParseInt(s.Value.String, 10, 64)
+		return val
+	case "float":
+		if s.Value == nil {
+			return 0.0
+		}
+		val, _ := strconv.ParseFloat(s.Value.String, 64)
+		return val
+	case "boolean":
+		if s.Value == nil {
+			return false
+		}
+		val, _ := strconv.ParseBool(s.Value.String)
+		return val
+	case "array":
+		if s.Value == nil {
+			return []string{}
+		}
+		if s.Value.String == "" {
+			return []string{}
+		}
+		// Assuming array values are comma-separated strings
+		vals := strings.Split(s.Value.String, ",")
+		return vals
+	default:
+		// Handle unknown type or default case
+		return nil
+	}
 }
