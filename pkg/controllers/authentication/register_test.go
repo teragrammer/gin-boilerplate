4e0d72dd6f37614d9a3d688eccbbc3d01ff93869
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-05-21 23:03:59 +0800

    Add registration unit testing

---
+++ pkg/controllers/authentication/register_test.go
@@ -0,0 +1,57 @@
+package authentication_test
+
+import (
+	"bytes"
+	"fmt"
+	"gin-boilerplate/database/migration"
+	"gin-boilerplate/pkg"
+	"gin-boilerplate/pkg/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var env = "test"
+var bootstrap = pkg.InitBoot("../../../env.json", &env)
+
+func TestRegisterHttp(t *testing.T) {
+	// Set Gin to Test mode
+	gin.SetMode(gin.TestMode)
+
+	// routes
+	routes.V1Routes(bootstrap)
+
+	// reset and delete test data
+	username := "test"
+	bootstrap.DB.Where("username = ?", username).Delete(&migration.User{})
+
+	// Create a new multipart writer
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// Add other fields
+	_ = writer.WriteField("first_name", "test")
+	_ = writer.WriteField("last_name", "test")
+	_ = writer.WriteField("username", username)
+	_ = writer.WriteField("password", "ABC&abc&123456")
+
+	// Close multipart writer
+	err := writer.Close()
+	if err != nil {
+		t.Error("Error closing writer:", err)
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/v1/register", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("X-Secret-Key", bootstrap.Env.App.Key)
+	bootstrap.Engine.ServeHTTP(w, req)
+
+	if http.StatusOK != w.Code {
+		fmt.Println("Err Body", w.Body.String())
+	}
+	assert.Equal(t, http.StatusOK, w.Code)
+}
