74b97e947d56735d7485e5b03ce8b9edf3c56f18
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-05-28 11:22:50 -0400

    Update error list

--- configs/errors.go
+++ configs/errors.go
@@ -16,6 +16,14 @@ type ErrorId struct {
 	E8  ErrorContent
 	E9  ErrorContent
 	E10 ErrorContent
+	E11 ErrorContent
+	E12 ErrorContent
+	E13 ErrorContent
+	E14 ErrorContent
+	E15 ErrorContent
+	E16 ErrorContent
+	E17 ErrorContent
+	E18 ErrorContent
 }
 
 func Errors() ErrorId {
@@ -30,5 +38,14 @@ func Errors() ErrorId {
 		E8:  ErrorContent{Code: "e8", Message: "Oops something went wrong during setting initialization"},
 		E9:  ErrorContent{Code: "e9", Message: "The information you are looking for is not available"},
 		E10: ErrorContent{Code: "e10", Message: "The system has detected too many incorrect login attempts"},
+		E11: ErrorContent{Code: "e11", Message: "Your account has been temporarily locked due to multiple login attempts"},
+
+		E12: ErrorContent{Code: "e12", Message: "Token format does not meet requirements"},
+		E13: ErrorContent{Code: "e13", Message: "The token has an incorrect length"},
+		E14: ErrorContent{Code: "e14", Message: "User information is incorrect"},
+		E15: ErrorContent{Code: "e15", Message: "The token detail entered is invalid"},
+		E16: ErrorContent{Code: "e16", Message: "The validity of the token is not valid"},
+		E17: ErrorContent{Code: "e17", Message: "Token timestamp could not be processed"},
+		E18: ErrorContent{Code: "e18", Message: "The token is no longer valid"},
 	}
 }
