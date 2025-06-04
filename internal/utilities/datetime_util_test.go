5c4360227b96491fd420d6da78e6d09ecdc09331
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-06-04 10:29:26 -0400

    Add unit test for adding minute to datetime

--- internal/utilities/datetime_util_test.go
+++ internal/utilities/datetime_util_test.go
@@ -93,3 +93,25 @@ func TestSubtractHour(t *testing.T) {
 
 	assert.Equal(t, parsedAddedDateTime.Format(layout), subtractedHours)
 }
+
+func TestAddMinute(t *testing.T) {
+	layout := "2006-01-02 15:04"
+	currentDateTime := "2023-06-15 13:10"
+	addedMinutes := "2023-06-15 13:15"
+
+	// Parse the MySQL datetime string into a time.Time object
+	parsedTime, err := time.Parse(layout, currentDateTime)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	addedDateTime := AddMinute(parsedTime, 5)
+	parsedAddedDateTime, err := time.Parse(layout, addedDateTime.Format(layout))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	assert.Equal(t, parsedAddedDateTime.Format(layout), addedMinutes)
+}
