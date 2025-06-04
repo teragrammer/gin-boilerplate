b65d6659c5719a2b18f3eb7a570d34f43545dd3b
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-06-04 10:28:30 -0400

    Add unit test for subtracting hour to datetime

--- internal/utilities/datetime_util_test.go
+++ internal/utilities/datetime_util_test.go
@@ -71,3 +71,25 @@ func TestAddHour(t *testing.T) {
 
 	assert.Equal(t, parsedAddedDateTime.Format(layout), addedHours)
 }
+
+func TestSubtractHour(t *testing.T) {
+	layout := "2006-01-02 15:04"
+	currentDateTime := "2023-06-15 10:45"
+	subtractedHours := "2023-06-15 08:45"
+
+	// Parse the MySQL datetime string into a time.Time object
+	parsedTime, err := time.Parse(layout, currentDateTime)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	addedDateTime := SubtractHour(parsedTime, 2)
+	parsedAddedDateTime, err := time.Parse(layout, addedDateTime.Format(layout))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	assert.Equal(t, parsedAddedDateTime.Format(layout), subtractedHours)
+}
