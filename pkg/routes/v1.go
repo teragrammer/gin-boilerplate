84a27bcd53f082af34c8fcfaec5e9e09ab6b5918
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-06-06 11:01:37 -0400

    Update v1 routes for tfa sending

--- pkg/routes/v1.go
+++ pkg/routes/v1.go
@@ -2,15 +2,21 @@ package routes
 
 import (
 	"gin-boilerplate/configs"
-	authentication2 "gin-boilerplate/pkg/controllers/authentication"
+	"gin-boilerplate/pkg/controllers/authentication"
 	"gin-boilerplate/pkg/middlewares"
 )
 
 func V1Routes(h configs.BootHandlers) {
 	api := h.Engine.Group("/v1")
 	{
-		api.POST("/register", middlewares.ApplicationKeyMiddleware(h), authentication2.Register(h))
-		api.POST("/login", middlewares.ApplicationKeyMiddleware(h), authentication2.Login(h))
-		api.GET("/logout", middlewares.ApplicationKeyMiddleware(h), middlewares.AuthenticateTokenMiddleware(h, true), authentication2.Logout(h))
+		api.POST("/register", middlewares.ApplicationKeyMiddleware(h), authentication.Register(h))
+		api.POST("/login", middlewares.ApplicationKeyMiddleware(h), authentication.Login(h))
+		api.GET("/logout", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), authentication.Logout(h))
+
+		api.POST("/tfa/send", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), authentication.NewTFAController(h).Send)
+		api.POST("/tfa/validate", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), authentication.NewTFAController(h).Validate)
 	}
 }
