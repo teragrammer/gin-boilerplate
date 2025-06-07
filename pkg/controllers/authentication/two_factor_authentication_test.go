4834c131a7ede3fa802f18d0e1c1bf5c26a6d9bf
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-06-07 11:46:17 -0400

    Create a test for tfa

---
+++ pkg/controllers/authentication/two_factor_authentication_test.go
@@ -0,0 +1,15 @@
+package authentication_test
+
+import (
+	"testing"
+)
+
+type TestTwoFactoAuthenticationModel struct {
+	token string
+}
+
+var TestTwoFactoAuthentication = TestTwoFactoAuthenticationModel{}
+
+func TestTwoFactoAuthenticationModelHttp(t *testing.T) {
+
+}
