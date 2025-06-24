68fea68fe8065e735ad3e706079abf17c27bea7d
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-06-24 11:00:49 -0400

    Add migration table for password recovery

--- database/migrate.go
+++ database/migrate.go
@@ -14,6 +14,7 @@ func RunMigrations() {
 		&migration.Setting{},
 		&migration.Role{},
 		&migration.User{},
+		&migration.PasswordRecovery{},
 		&migration.AuthenticationToken{},
 		&migration.TwoFactorAuthentication{},
 	)
