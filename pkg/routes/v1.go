7185270bcbcd0ac013557f2570457c9994cb753d
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-07-03 11:10:49 -0400

    Add new routes for settings

--- pkg/routes/v1.go
+++ pkg/routes/v1.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"gin-boilerplate/configs"
+	"gin-boilerplate/pkg/controllers/application"
 	"gin-boilerplate/pkg/controllers/authentication"
+	"gin-boilerplate/pkg/controllers/user"
 	"gin-boilerplate/pkg/middlewares"
 )
 
@@ -22,7 +24,20 @@ func V1Routes(h configs.BootHandlers) {
 		api.POST("/password-recovery/send", middlewares.ApplicationKeyMiddleware(h), authentication.NewPasswordRecoveryController(h).Send)
 		api.POST("/password-recovery/validate", middlewares.ApplicationKeyMiddleware(h), authentication.NewPasswordRecoveryController(h).Validate)
 
-		api.PATCH("/account/information", middlewares.ApplicationKeyMiddleware(h), middlewares.AuthenticateTokenMiddleware(h, true), authentication.NewAccountController(h).Information)
-		api.PATCH("/account/password", middlewares.ApplicationKeyMiddleware(h), middlewares.AuthenticateTokenMiddleware(h, true), authentication.NewAccountController(h).Password)
+		api.PATCH("/account/information", middlewares.ApplicationKeyMiddleware(h), middlewares.AuthenticateTokenMiddleware(h, true), user.NewAccountController(h).Information)
+		api.PATCH("/account/password", middlewares.ApplicationKeyMiddleware(h), middlewares.AuthenticateTokenMiddleware(h, true), user.NewAccountController(h).Password)
+
+		api.GET("/settings", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), application.NewSettingController(h).Browse)
+		api.GET("/settings/values", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), application.NewSettingController(h).Values)
+		api.GET("/settings/:id", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), application.NewSettingController(h).View)
+		api.POST("/settings", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), application.NewSettingController(h).Create)
+		api.PATCH("/settings", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), application.NewSettingController(h).Update)
+		api.DELETE("/settings/:id", middlewares.ApplicationKeyMiddleware(h),
+			middlewares.AuthenticateTokenMiddleware(h, true), application.NewSettingController(h).Delete)
 	}
 }
