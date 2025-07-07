263d774a89199eefd3729eaf28122eac7b4ef4b8
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-07-07 11:01:29 -0400

    Add create, update and delete for settings controller

--- pkg/controllers/application/setting.go
+++ pkg/controllers/application/setting.go
@@ -1,8 +1,10 @@
 package application
 
 import (
+	"database/sql"
 	"gin-boilerplate/configs"
 	"gin-boilerplate/database/migration"
+	"gin-boilerplate/internal/handlers"
 	"gin-boilerplate/internal/utilities"
 	"gin-boilerplate/pkg/repositories"
 	"github.com/gin-gonic/gin"
@@ -94,13 +96,127 @@ func (controller *SettingController) View(c *gin.Context) {
 }
 
 func (controller *SettingController) Create(c *gin.Context) {
+	var form struct {
+		Name        string  `form:"name" validate:"required,min=1,max=100" json:"name"`
+		Slug        string  `form:"slug" validate:"required,min=1,max=100" json:"slug"`
+		Value       *string `form:"value" validate:"omitempty" json:"value"`
+		Description *string `form:"description" validate:"omitempty,min=1,max=200" json:"description"`
+		Type        string  `form:"type" validate:"required,oneof=string integer float boolean array" json:"type"`
+		IsDisabled  int     `form:"is_disabled" validate:"required,oneof=0 1" json:"is_disabled"`
+		IsPublic    int     `form:"is_public" validate:"required,oneof=0 1" json:"is_public"`
+	}
 
+	e := handlers.ValidationHandler(c, &form)
+	if e != nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, e)
+		return
+	}
+
+	// check if slug already exists
+	var _setting migration.Setting
+	if err := controller.h.DB.Where("slug", form.Slug).First(&_setting).Error; err == nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    configs.Errors().E5.Code,
+			"message": configs.Errors().E5.Message,
+		})
+		return
+	}
+
+	var isDisabled = form.IsPublic == 1
+	var isPublic = form.IsPublic == 1
+
+	var setting = migration.Setting{
+		Name:        form.Name,
+		Slug:        form.Slug,
+		Value:       utilities.ValueOfNullString(form.Value),
+		Description: utilities.ValueOfNullString(form.Description),
+		Type:        form.Type,
+		IsDisabled:  &utilities.NullBool{NullBool: sql.NullBool{Valid: true, Bool: isDisabled}},
+		IsPublic:    &utilities.NullBool{NullBool: sql.NullBool{Valid: true, Bool: isPublic}},
+	}
+	if err := controller.h.DB.
+		Create(setting).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"code":    configs.Errors().E7.Code,
+			"message": configs.Errors().E7.Message,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, setting.Id)
 }
 
 func (controller *SettingController) Update(c *gin.Context) {
+	var form struct {
+		Name        string  `form:"name" validate:"required,min=1,max=100" json:"name"`
+		Slug        string  `form:"slug" validate:"required,min=1,max=100" json:"slug"`
+		Value       *string `form:"value" validate:"omitempty" json:"value"`
+		Description *string `form:"description" validate:"omitempty,min=1,max=200" json:"description"`
+		Type        string  `form:"type" validate:"required,oneof=string integer float boolean array" json:"type"`
+		IsDisabled  int     `form:"is_disabled" validate:"required,oneof=0 1" json:"is_disabled"`
+		IsPublic    int     `form:"is_public" validate:"required,oneof=0 1" json:"is_public"`
+	}
 
+	e := handlers.ValidationHandler(c, &form)
+	if e != nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, e)
+		return
+	}
+
+	// check if slug already exists
+	id := c.Param("id")
+	var _setting migration.Setting
+	if err := controller.h.DB.
+		Where("id <> ?", id).
+		Where("slug", form.Slug).
+		First(&_setting).Error; err == nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    configs.Errors().E5.Code,
+			"message": configs.Errors().E5.Message,
+		})
+		return
+	}
+
+	var isDisabled = form.IsPublic == 1
+	var isPublic = form.IsPublic == 1
+
+	var setting = migration.Setting{
+		Name:        form.Name,
+		Slug:        form.Slug,
+		Value:       utilities.ValueOfNullString(form.Value),
+		Description: utilities.ValueOfNullString(form.Description),
+		Type:        form.Type,
+		IsDisabled:  &utilities.NullBool{NullBool: sql.NullBool{Valid: true, Bool: isDisabled}},
+		IsPublic:    &utilities.NullBool{NullBool: sql.NullBool{Valid: true, Bool: isPublic}},
+	}
+	if err := controller.h.DB.
+		Where("id = ?", id).
+		Updates(setting).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"code":    configs.Errors().E7.Code,
+			"message": configs.Errors().E7.Message,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": true,
+	})
 }
 
 func (controller *SettingController) Delete(c *gin.Context) {
+	id := c.Param("id")
+	if err := controller.h.DB.
+		Where("id = ?", id).
+		Delete(&migration.AuthenticationToken{}).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"code":    configs.Errors().E7.Code,
+			"message": configs.Errors().E7.Message,
+		})
+		return
+	}
 
+	c.JSON(http.StatusOK, gin.H{
+		"result": true,
+	})
 }
