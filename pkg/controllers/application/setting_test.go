12f845ea5f970c344d03d1e461fe64df6d479be7
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-07-11 11:32:16 -0400

    Remove unused test

--- pkg/controllers/application/setting_test.go
+++ pkg/controllers/application/setting_test.go
@@ -25,7 +25,7 @@ type TestSettingModel struct {
 
 var TestSetting = TestSettingModel{}
 
-func TestAccountSetup(t *testing.T) {
+func TestSettingSetup(t *testing.T) {
 	var env = "test"
 	var bootstrap = pkg.InitBoot("../../../env.json", &env)
 
@@ -42,49 +42,6 @@ func TestAccountSetup(t *testing.T) {
 	var user, token = tester.GenerateAuthentication(bootstrap, "admin", "admin_test")
 	TestSetting.user = user
 	TestSetting.token = token
-}
-
-func TestAccountInformationHttp(t *testing.T) {
-	var env = "test"
-	var bootstrap = pkg.InitBoot("../../../env.json", &env)
-
-	// Set Gin to Test mode
-	gin.SetMode(gin.TestMode)
-
-	// routes
-	routes.V1Routes(bootstrap)
-
-	// Create a new multipart writer
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	// Add other fields
-	_ = writer.WriteField("first_name", "User")
-	_ = writer.WriteField("last_name", "One")
-
-	// Close multipart writer
-	err := writer.Close()
-	if err != nil {
-		t.Error("Error closing writer:", err)
-	}
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("PATCH", "/v1/account/information", body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	req.Header.Set("X-Secret-Key", bootstrap.Env.App.Key)
-	req.Header.Set("Authorization", TestSetting.token.Token)
-	bootstrap.Engine.ServeHTTP(w, req)
-
-	if http.StatusOK != w.Code {
-		fmt.Println("Err Body", w.Body.String())
-	}
-
-	var user = migration.User{}
-	bootstrap.DB.Where("id = ?", TestSetting.user.Id).First(&user)
-
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "User", user.FirstName)
-	assert.Equal(t, "One", user.LastName.String)
 }
 
 func TestCreateSettingHttp(t *testing.T) {
