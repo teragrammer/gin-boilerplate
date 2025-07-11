a1e0b40886c7af4a3926eee44ea3dc67fad423bc
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-07-11 11:43:51 -0400

    Add unit to browse roles

--- pkg/controllers/application/role_test.go
+++ pkg/controllers/application/role_test.go
@@ -160,3 +160,25 @@ func TestUpdateRoleHttp(t *testing.T) {
 	assert.Equal(t, "test_new", role.Name)
 	assert.Equal(t, false, role.IsActive.Bool)
 }
+
+func TestRoleBrowseHttp(t *testing.T) {
+	var env = "test"
+	var bootstrap = pkg.InitBoot("../../../env.json", &env)
+
+	// Set Gin to Test mode
+	gin.SetMode(gin.TestMode)
+
+	// routes
+	routes.V1Routes(bootstrap)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/roles", nil)
+	req.Header.Set("X-Secret-Key", bootstrap.Env.App.Key)
+	req.Header.Set("Authorization", TestRole.token.Token)
+	bootstrap.Engine.ServeHTTP(w, req)
+
+	if http.StatusOK != w.Code {
+		fmt.Println("Err Body", w.Body.String())
+	}
+	assert.Equal(t, http.StatusOK, w.Code)
+}
