08580aeb6ad1b8c0a07ffc3e0d65b7fa9bdfc6a0
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-07-14 10:58:27 -0400

    Add unit test to view user information

--- pkg/controllers/user/user_test.go
+++ pkg/controllers/user/user_test.go
@@ -175,3 +175,25 @@ func TestUserBrowseHttp(t *testing.T) {
 	}
 	assert.Equal(t, http.StatusOK, w.Code)
 }
+
+func TestUserViewHttp(t *testing.T) {
+	var env = "test"
+	var bootstrap = pkg.InitBoot("../../../env.json", &env)
+
+	// Set Gin to Test mode
+	gin.SetMode(gin.TestMode)
+
+	// routes
+	routes.V1Routes(bootstrap)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/users/"+strconv.Itoa(int(TestUser.id)), nil)
+	req.Header.Set("X-Secret-Key", bootstrap.Env.App.Key)
+	req.Header.Set("Authorization", TestUser.token.Token)
+	bootstrap.Engine.ServeHTTP(w, req)
+
+	if http.StatusOK != w.Code {
+		fmt.Println("Err Body", w.Body.String())
+	}
+	assert.Equal(t, http.StatusOK, w.Code)
+}
